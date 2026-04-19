cb88a02ed700db43195fa7926de0fd83fe8d30a3
Gaurav-Chavan22 <a250049@famt.ac.in>
2026-04-19 22:43:03 +0530

    First Commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Table of 10");
+	for(int i=0;i<10;i++){
+		System.out.println("10*"+i+" = "+10*i);
+	}
+    }
+}
